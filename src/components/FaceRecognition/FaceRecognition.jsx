094dcfe51892664b2fa964095940787ed290c17e
Eleni Terzi <Eleni@macbookair.home>
2025-03-10 15:45:48 +0100

    facecognition - detection box

--- src/components/FaceRecognition/FaceRecognition.jsx
+++ src/components/FaceRecognition/FaceRecognition.jsx
@@ -1,11 +1,26 @@
 import  React from 'react'; 
 import './FaceRecognition.css';
 
-function FaceRecognition({imageUrl}) {
+function FaceRecognition({imageUrl, boxes}) {
+    console.log("Received boxes:", boxes); // Check console for output
     return (
         <div className='center ma'>
-            <div className='absolute mt2'>
-                <img alt='' src={imageUrl} width='400px' height='auto'/>
+            <div className='absolute mt2' style={{ position: "relative" }}>
+                <img src={imageUrl} width='400px' height='auto'/>
+                {boxes.map((box, index) => {
+                    return (
+                        <div key={index} className='bounding-box'
+                            style={{
+                                position: "absolute",
+                                top: `${box.topRow * 100}%`,
+                                left: `${box.leftCol * 100}%`,
+                                width: `${(box.rightCol - box.leftCol) * 100}%`,
+                                height: `${(box.bottomRow - box.topRow) * 100}%`
+                            }}
+                          
+                        />
+                    );
+                })}
          </div>
         </div>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,121 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React, {Component, StrictMode} from "react";
+import { createRoot } from "react-dom/client";
+import Navigation from "./components/Navigation/Navigation.jsx";
+import Logo from "./components/Logo/Logo.jsx";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm.jsx";
+import Rank from "./components/Rank/Rank.jsx";
+import ParticlesBg from 'particles-bg';
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition.jsx";
+import './index.css';
 import "tachyons/css/tachyons.min.css";
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+const returnClarifaiRequestOptions = (imageUrl) => {
+  const PAT = 'f267ee1270f74435a126fb33851fb86c';
+  const USER_ID = 'elen23_cyberella';
+  const APP_ID = 'facerecognitionbrain';
+  const MODEL_ID = 'face-detection';
+  const IMAGE_URL = imageUrl;
+
+  const raw = JSON.stringify({
+    user_app_id: {
+      user_id: USER_ID,
+      app_id: APP_ID
+    },
+    inputs: [
+      {
+        data: {
+          image: {
+            url: imageUrl
+          }
+        }
+      }
+    ]
+  });
+  
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Authorization': 'Key ' + PAT
+    },
+    body: raw
+  };
+  return requestOptions
+};
+
+
+class App extends Component {
+  constructor() {   
+    super();    //Calls the parent class (Component) constructor to enable "this".
+    this.state = {     //Sets up the initial state of the component.
+      input: "",     // that is what the user will input
+      imageUrl: "",   // should get displayed when we click onButtonSubmit
+      boxes : []  //Stores detected face bounding box coordinates.
+
+    };
+  } 
+
+  onInputChange = (event) => {    // It's a prop of the App and will be called when the input field changes, we receive an event
+    this.setState({ input: event.target.value });  //Updates the state of the component with the value of the input field.
+  } 
+
+  onButtonSubmit = () => {    //This function is called when the submit button is clicked.
+    const IMAGE_URL = this.state.input;   //Stores the value of the input field in a variable.
+    console.log('About to fetch Clarifai API');
+    this.setState({ imageUrl: IMAGE_URL });   //Updates imageUrl in the state to display the submitted image.
+
+
+    //fetch - sends a POST request to Clarifai’s API with the image URL.
+    fetch("/clarifai/v2/models/"+ "face-detection" + "/outputs", returnClarifaiRequestOptions(this.state.input))
+      .then(response => response.json())
+      .then(result => {
+          console.log(result);   //Logs the API response to the console.
+
+                         //Extracts first output from the API response[] and then gets face detection regions
+          const regions = result.outputs[0].data.regions;
+          const boxes = regions.map(region => {   //Loops through and maps the detected face regions to an array of bounding boxes.
+            const boundingBox = region.region_info.bounding_box;
+            return {
+              topRow: parseFloat (boundingBox.top_row.toFixed(3)),     //parseFloat - converts rounded values to numbers
+              leftCol: parseFloat (boundingBox.left_col.toFixed(3)),   // toFixed - rounds the value to 3 decimal places
+              bottomRow: parseFloat (boundingBox.bottom_row.toFixed(3)),
+              rightCol: parseFloat (boundingBox.right_col.toFixed(3))
+            }
+          }
+          );
+          this.setState({ boxes });  //Updates the state with the detected face bounding boxes.
+
+
+          regions.forEach(region => {    // Logs face detection concepts & confidence and prints them to the console.
+              region.data.concepts.forEach(concept => {
+                  // Accessing and rounding the concept value
+                  const name = concept.name;
+                  const value = concept.value.toFixed(4);
+
+                  console.log(`${name}: ${value}`);  
+              });
+          });
+      })
+     .catch(error => console.log('error',error));
+  }
+
+  render() {
+    return (
+      <StrictMode>
+          <ParticlesBg type="circle" bg={true} />
+          <Navigation />
+          <Logo />
+          <Rank />
+          <ImageLinkForm  
+            onInputChange={this.onInputChange} 
+            onButtonSubmit={this.onButtonSubmit}/>    
+          <FaceRecognition 
+            imageUrl={this.state.imageUrl}
+            boxes={this.state.boxes}/>
+      </StrictMode>
+    );
+  }
+}  
+
+createRoot(document.getElementById('root')).render (<App />);
